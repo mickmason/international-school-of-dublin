7cc9217c16fce98652381ac08cfc002298a2ff8c
mickmason <mmason2@hotmail.com>
2020-01-18 15:12:21 +0000

    Adding github pages docs/ folder

---
+++ docs/scripts/app-concat.js
@@ -0,0 +1,35 @@
+/* Lib 1 file */
+cons = ('Hello') => {
+	console.log(`Hello`);
+}/* app.js */
+function sayHello(name) {
+	console.log(`Hello ${name}`);
+}
+sayHello(); /* app.js */
+function sayHello(name) {
+	console.log(`Hello ${name}`);
+}
+sayHello(); /* Lib 1 file */
+cons = ('Hello') => {
+	console.log(`Hello`);
+}/* Lib 1 file */
+const howdy = (hi) => {
+	console.log(hi);
+};
+howdy('Hello');"use strict";
+
+/* App.js */
+function sayHello(name) {
+  console.log("Hello ".concat(name));
+}
+
+sayHello();
+/* Lib 1 file */
+const howdy = (hi) => {
+	console.log(hi);
+};
+howdy('Hello');"use strict";function sayHello(name){console.log("Hello ".concat(name))}sayHello();/* Lib 1 file */
+const howdy = (hi) => {
+	console.log(hi);
+};
+howdy('Hello');"use strict";function sayHello(name){console.log("Hello ".concat(name))}sayHello();
